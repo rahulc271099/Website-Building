4445f961116e335d2e53e601ceedbf4dbef08eff
rahulc271099 <rahulc271099@gmail.com>
2024-12-29 16:19:48 +0530

    Created Fictional Website

---
+++ script.js
@@ -0,0 +1,34 @@
+document.querySelector('#submit-btn').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const email = document.getElementById('floatingEmail').value
+    const phone = document.getElementById('floatingPhone').value
+    const text = document.getElementById('floatingText').value
+    const messege = document.getElementById('floatingTextarea').value
+
+    if(!email || !phone || !text || !messege){
+        alert("Please fill out the field")
+        return
+    }
+    const formData = {
+        email:email,
+        phone:phone,
+        text:text,
+        messege:messege
+    }
+
+    localStorage.setItem('contactFormData',JSON.stringify(formData))
+
+    if(localStorage.getItem){
+        alert("Form Submitted")
+    }else{
+        alert("Error")
+    }
+
+
+    // clear the input field
+    document.getElementById('floatingEmail') = ""
+    document.getElementById('floatingPhone') = ""
+    document.getElementById('floatingText') = ""
+    document.getElementById('floatingTextarea') = ""
+})
